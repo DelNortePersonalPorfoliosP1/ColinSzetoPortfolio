e8137a0f06628782e563a908b8884005f59c8949
Colin Szeto <kolinszeto@gmail.com>
2019-09-13 08:20:24 -0700

    added comments
    added comment

---
+++ Lab1/Lab1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab1
+//
+//  Created by Szeto, Colin on 9/13/19.
+//  Copyright © 2019 Szeto, Colin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+ // comment change
+    
+}
